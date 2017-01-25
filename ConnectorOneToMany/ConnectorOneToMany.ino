ce789b4f63f6f6c15f67e4b8aee88764d7a16a67
Asuki Kono <asukiaaa@gmail.com>
2017-01-25 21:59:36 +0900

    Change pin and add a serial port for ConnectorOneToMany

--- ConnectorOneToMany/ConnectorOneToMany.ino
+++ ConnectorOneToMany/ConnectorOneToMany.ino
@@ -4,14 +4,18 @@
 //#define DEBUG_MODE
 
 HaLakeKitFirstConnector kitConnector(&Serial1);
-SoftwareSerial someSerial1(6,7);
-SoftwareSerial someSerial2(8,9);
+SoftwareSerial someSerial1(2,3);
+SoftwareSerial someSerial2(4,5);
+SoftwareSerial someSerial3(7,8);
 
 String kitStr;
 String strToSend;
 
 void setup() {
   kitConnector.begin();
+  someSerial1.begin(HALAKEKITFIRST_SERIAL_SPEED);
+  someSerial2.begin(HALAKEKITFIRST_SERIAL_SPEED);
+  someSerial3.begin(HALAKEKITFIRST_SERIAL_SPEED);
 #ifdef DEBUG_MODE
   while (!Serial);
   Serial.begin(115200);
@@ -25,6 +29,7 @@ void loop() {
     strToSend = kitStr + "\n";
     someSerial1.print(strToSend);
     someSerial2.print(strToSend);
+    someSerial3.print(strToSend);
 #ifdef DEBUG_MODE
     Serial.print(strToSend);
 #endif
