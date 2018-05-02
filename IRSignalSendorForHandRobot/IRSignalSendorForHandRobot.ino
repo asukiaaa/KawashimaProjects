1cb8b5dff172f915b7ccf1123ec27b5830402020
Asuk Kono <asukiaaa@gmail.com>
2018-05-02 23:14:36 +0900

    Update IR info of IRSignalSendorForHandRobot with using IRremote

--- IRSignalSendorForHandRobot/IRSignalSendorForHandRobot.ino
+++ IRSignalSendorForHandRobot/IRSignalSendorForHandRobot.ino
@@ -1,37 +1,20 @@
-#define IR_SEND_PIN A0
+#include <IRremote.h>
 
-int signalA[] = {906,442,62,50,62,50,62,50,62,50,62,162,62,50,61,50,62,51,62,162,61,162,62,162,62,162,62,50,61,162,62,161,62,162,62,50,61,50,62,50,61,162,61,162,62,161,62,162,62,162,62,162,62,162,62,161,62,50,62,50,61,50,61,51,61,52,59,3959,908,218,61};
-int signalB[] = {908,440,65,47,65,47,65,47,64,47,65,158,64,48,64,47,64,48,64,159,64,159,64,158,64,159,64,48,64,158,64,159,64,160,64,48,64,48,64,48,63,162,61,160,62,160,63,162,60,50,64,159,62,160,63,160,63,52,61,48,64,49,62,49,62,162,60,3959,908,218,61};
-int signalC[] = {908,440,62,49,62,50,62,50,62,50,62,162,61,50,62,50,62,51,61,162,61,162,62,161,62,161,62,50,62,162,61,162,61,162,62,50,61,50,62,50,62,162,61,162,62,50,61,161,62,51,62,162,61,162,62,162,62,50,61,50,61,162,61,50,62,160,62,3959,908,218,61};
+IRsend irsend; // Send pin is 3 for atmega328(Uno, Nano) and 13 for atmega23u4 (Leonardo, Micro)
+const unsigned int signalA = 0x8F71FE0;
+const unsigned int signalB = 0x8F71EE1;
+const unsigned int signalC = 0x8F71AE5;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(IR_SEND_PIN, OUTPUT);
 }
 
 void loop() {
   delay(2000);
-  sendSignal(signalA);
+  irsend.sendNEC(signalA, 32);
   delay(2000);
-  sendSignal(signalB);
+  irsend.sendNEC(signalB, 32);
   delay(2000);
-  sendSignal(signalC);
+  irsend.sendNEC(signalC, 32);
+  delay(2000);
 }
-
-void sendSignal(int signalArray[]) {
-  bool currentState = false;
-  int i;
-  for (i=0; i<(sizeof(signalArray)/sizeof(int)); i++) {
-    currentState = !currentState;
-    if (currentState) {
-      digitalWrite(IR_SEND_PIN, HIGH);
-    } else {
-      digitalWrite(IR_SEND_PIN, LOW);
-    }
-    delayMicroseconds(signalArray[i]);
-  }
-  if (currentState) {
-    digitalWrite(IR_SEND_PIN, LOW);
-  }
-}
-
